ce2d8bbc764067902e7f6246c06cbcbc33d551af
newt <hi@newty.dev>
2025-02-22 20:36:49 +0000

    feat: check if system muted

--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module newty.dev/muter
 
 go 1.24.0
 
-require github.com/kardianos/service v1.2.2
+require (
+	github.com/go-ole/go-ole v1.3.0
+	github.com/kardianos/service v1.2.2
+	github.com/moutend/go-wca v0.3.0
+)
 
-require golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
+require golang.org/x/sys v0.1.0 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/go-ole/go-ole"
 	"github.com/kardianos/service"
+	"github.com/moutend/go-wca/pkg/wca"
 )
 
 const (
@@ -16,35 +18,69 @@ type muter struct {
 }
 
 func main() {
-	// create service
-	m := &muter{
-		exitChan: make(chan struct{}),
-	}
-	svc, err := service.New(m, &service.Config{
-		Name: ServiceName,
-	})
-	if err != nil {
+	// // create service
+	// m := &muter{
+	// 	exitChan: make(chan struct{}),
+	// }
+	// svc, err := service.New(m, &service.Config{
+	// 	Name: ServiceName,
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // collect errors
+	// errorChan := make(chan error, 5)
+	// m.logger, err = svc.Logger(errorChan)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// go func() {
+	// 	for {
+	// 		err := <-errorChan
+	// 		if err != nil {
+	// 			log.Print(err)
+	// 		}
+	// 	}
+	// }()
+
+	// // start service
+	// if err := svc.Run(); err != nil {
+	// 	m.logger.Error(err)
+	// }
+
+	// initialize COM
+	if err := ole.CoInitializeEx(0, ole.COINIT_MULTITHREADED); err != nil {
 		log.Fatal(err)
 	}
+	defer ole.CoUninitialize()
 
-	// collect errors
-	errorChan := make(chan error, 5)
-	m.logger, err = svc.Logger(errorChan)
-	if err != nil {
+	// get audio device enumerator
+	var mmde *wca.IMMDeviceEnumerator
+	if err := wca.CoCreateInstance(wca.CLSID_MMDeviceEnumerator, 0, wca.CLSCTX_ALL, wca.IID_IMMDeviceEnumerator, &mmde); err != nil {
 		log.Fatal(err)
 	}
+	defer mmde.Release()
 
-	go func() {
-		for {
-			err := <-errorChan
-			if err != nil {
-				log.Print(err)
-			}
-		}
-	}()
-
-	// start service
-	if err := svc.Run(); err != nil {
-		m.logger.Error(err)
+	// get default capture device
+	var mmd *wca.IMMDevice
+	if err := mmde.GetDefaultAudioEndpoint(wca.ECapture, wca.EMultimedia, &mmd); err != nil {
+		log.Fatal(err)
 	}
+	defer mmd.Release()
+
+	// activate audio endpoint volume
+	var aev *wca.IAudioEndpointVolume
+	if err := mmd.Activate(wca.IID_IAudioEndpointVolume, wca.CLSCTX_ALL, nil, &aev); err != nil {
+		log.Fatal(err)
+	}
+	defer aev.Release()
+
+	// get initial mute state
+	var muted bool
+	if err := aev.GetMute(&muted); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("initial mute state: %v", muted)
 }
